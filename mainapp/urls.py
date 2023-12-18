38ca623fb40da663f0d1a23fe10a979a9a30406b
ecrvmal <ecrvmal@yandex.ru>
2023-12-19 01:49:29 +0300

    Native_Implementation_2 preliminary

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-
 from mainapp import views
+from mainapp.apps import MainappConfig
 
-app_name = "mainapp"
+app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="index"),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import TemplateView
-from mainapp.models import Record, Category
-
-previous_record = ''
+from mainapp.apps import MainappConfig
+from mainapp.models import Record
 
 
 class MainPageView(TemplateView):
@@ -16,21 +15,13 @@ class MainPageView(TemplateView):
         :param **kwargs: Pass keyworded, variable-length argument list
         :return: A context dictionary
         """
-        global previous_record
-
-        all_categs = Category.objects.all()
-        all_categs_dict = {}
-        for el in all_categs:
-            # print(el.id, el.name)
-            all_categs_dict[el.name] = int(el.id)
-
         reqv_category_list_names = self.request.GET.getlist('category')
         reqv_category_list_id = []
         try:
-            reqv_category_list_id = [all_categs_dict[item]
+            reqv_category_list_id = [MainappConfig.all_categs_dict[item]
                                      for item in reqv_category_list_names]
         except KeyError as e:
-            print(e)
+            print(f'key not found in dict {e}')
         context = super().get_context_data(**kwargs)
         list1 = Record.objects.all().\
             filter(category__in=reqv_category_list_id).\
@@ -44,9 +35,9 @@ class MainPageView(TemplateView):
         context['item'] = ""
         if list1:
             for record in list1:
-                if record.file_url != previous_record and record.num_of_show > 0:
+                if record.file_url != self.request.session.get('previous_record') and record.num_of_show > 0:
                     context['item'] = record
-                    previous_record = record.file_url
+                    self.request.session['previous_record'] = record.file_url
                     record.num_of_show -= 1
                     record.save(update_fields=["num_of_show",])
                     break
